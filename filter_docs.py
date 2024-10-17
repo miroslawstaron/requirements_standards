10dc7ab2c3d8b24e5f02cddc5a5fb882d77b040c
schonda <schonda@student.chalmers.se>
2024-10-17 14:40:56 +0200

    #12 add config.json

--- filter_docs.py
+++ filter_docs.py
@@ -1,3 +1,5 @@
+# import the necessary libraries
+# we should probably change this 
 import os
 import csv
 from docx import Document
@@ -6,10 +8,10 @@ import json
 import re
 
 def ask_llm(paragraph):
-    url = 'http://localhost:11434/api/generate'
+    url = 'http://localhost:11435/api/generate'
     data = {
         "model": "llama3.1:70b",
-        "prompt": prompts['verify_context'],
+        "prompt": prompts['verify_context'] + paragraph,
         "stream": False
         }
     headers = {'Content-Type': 'application/json'}
@@ -17,7 +19,7 @@ def ask_llm(paragraph):
     json_data = json.loads(response.text)
     return json_data['response']
 
-def extract_paragraphs_with_keywords(doc, keywords, filename):
+def extract_paragraphs_with_keywords(doc, keywords, filename, config):
     paragraphs = []
     requirements = []
     current_section = ""
@@ -30,7 +32,7 @@ def extract_paragraphs_with_keywords(doc, keywords, filename):
         # Check if the paragraph contains any of the keywords
         if any(keyword.lower() in paragraph.text.lower() for keyword in keywords):
             # Check if the section is not ignored
-            if current_section != "" and not any(ignored_section in current_section for ignored_section in ignored_sections):
+            if current_section != "" and not any(ignored_section in current_section for ignored_section in config['ignored_sections']):
                 paragraphs.append((filename, current_section, paragraph.text))
         # Check if the paragraph contains a ms unit
         elif unit_regex.search(paragraph.text):
@@ -42,7 +44,7 @@ def extract_paragraphs_with_keywords(doc, keywords, filename):
     return paragraphs, requirements
 
 
-def process_docx_files_in_folder(folder_path, search_word, output_csv):
+def process_docx_files_in_folder(folder_path, search_word, output_csv, config):
     requirements = []
     with open(output_csv, 'w', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=';')
@@ -52,7 +54,7 @@ def process_docx_files_in_folder(folder_path, search_word, output_csv):
                 file_path = os.path.join(folder_path, filename)
                 print(f"Processing file: {file_path}")
                 doc = Document(file_path)
-                found_paragraphs, found_requirements = extract_paragraphs_with_keywords(doc, search_word, filename)
+                found_paragraphs, found_requirements = extract_paragraphs_with_keywords(doc, search_word, filename, config)
                 requirements.extend(found_requirements)
                 for filename, section, paragraph in found_paragraphs:
                     csvwriter.writerow([filename[:-5], section, paragraph, ask_llm(paragraph)])
@@ -62,12 +64,14 @@ def process_docx_files_in_folder(folder_path, search_word, output_csv):
         for filename, section, paragraph in requirements:
             csvwriter.writerow([filename[:-5], section, paragraph, ask_llm(paragraph)])
 
-with open('prompts.json', 'r') as f:
-    prompts = json.load(f)
+def execute_filtering(config):
+    folder_path = config['folder_path']
+    keywords = config['keywords']
+    output_csv = config['output_csv']
+    process_docx_files_in_folder(folder_path, keywords, output_csv, config)
 
-folder_path = "standards/22_standards"
-keywords = ["latency", "latencies"]
-ignored_sections = ["References", "Appendix", "Definitions", "Abbreviations"]
-output = "outputs/latency_paragraphs.csv"
+########################################################################
+# main function (executed when running this file)
 
-process_docx_files_in_folder(folder_path, keywords, output)
+if __name__ == "__main__":
+    print("skibidi")
