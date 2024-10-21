faeaa039c799e4f77ade0a5791bf9a693262dc54
Adrian Hassa <adrianhassa@gmail.com>
2024-10-21 14:21:44 +0200

    #12 Add model selection

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,7 @@
 import json
 import os
 import tkinter as tk
+from tkinter import ttk
 import filter_docs
 
 # File path for the configuration file
@@ -21,22 +22,26 @@ root = tk.Tk()
 root.title(config.get("title", "Generate Requirements"))
 
 path_var=tk.StringVar(value=config['folder_path'])
-output_var=tk.StringVar(value=config['output_csv'])
+output_var=tk.StringVar(value=config['latency_paragraphs'])
 keyword_var=tk.StringVar(value=config['keywords'][0])
+model_var=config['model_name']
 
 def run():
     config = {
         'folder_path': path_var.get(),
-        'output_csv': output_var.get(),
+        'latency_paragraphs': output_var.get(),
         'keywords': [keyword_var.get()],
         'ignored_sections': ["References", "Appendix", "Definitions", "Abbreviations"],
-        'model_name': "llama3.1:70b",
+        'model_name': model_var,
         'verbose': False
     }
     #save config to config.json
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f)
     filter_docs.execute_filtering(config)
+
+def on_model_select(event):
+    model_var = combo_box.get()
 
 # Set window size
 width = config.get("width", 500)
@@ -52,6 +57,10 @@ output_entry=tk.Entry(root, textvariable = output_var, font = ('arial',10,'norma
 keyword_label = tk.Label(root, text = 'Keyword', font = ('arial',10,'normal'))
 keyword_entry=tk.Entry(root, textvariable = keyword_var, font = ('arial',10,'normal'), width=50)
 
+model_label = tk.Label(root, text = 'Language Model', font = ('arial',10,'normal'))
+model_entry=ttk.Combobox(root, values=["llama3.1", "llama3.1:70b", "llama3.1:405b"], width=50)
+model_entry.set(model_var)
+
 run_btn=tk.Button(root,text = 'Run', command = run, width=30)
 
 path_label.grid(row=0,column=0, padx=5)
@@ -60,7 +69,9 @@ output_label.grid(row=1,column=0, padx=5)
 output_entry.grid(row=1,column=1, pady=10)
 keyword_label.grid(row=2,column=0, padx=5)
 keyword_entry.grid(row=2,column=1, pady=10)
-run_btn.grid(row=3,column=1)
+model_label.grid(row=3,column=0, padx=5)
+model_entry.grid(row=3,column=1, pady=10)
+run_btn.grid(row=4,column=1)
 
 # Run the application
 root.mainloop()
